7684642747be18c2e5b50fd9c9b8159911d0f896
Henrique <henriquesg09@hotmail.com>
2019-11-24 14:43:52 -0300

    Feature/MVV(frontend e backend)

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class MVV(models.Model):
+    missao_texto = models.TextField('Texto para descrever a missão da empresa', max_length=200, null=False,);
+    visao_texto = models.TextField('Texto para descrever a visão da empresa', max_length=200, null=False);
+    valor_texto = models.TextField('Texto para descrever os valores da empresa', max_length=200, null=False);
+
+    class Meta:
+        verbose_name = 'MVV'   
+        verbose_name_plural = 'MVV'   
+
+    def __str__(self):
+        return "Sobre a Biotech"
--- core/views.py
+++ core/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from core.models import *
+
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        context["mvv"] = MVV.objects.last()
+
+        return context
