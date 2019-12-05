57da5f5f740275dec34e9e23b2098dac28968225
Henrique <henriquesg09@hotmail.com>
2019-12-05 04:58:41 -0300

    Fixed bugs

--- biotech/settings/base.py
+++ biotech/settings/base.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.biotechconsultoria.herokuapp.com', '.biotechconsultoria.com', '.biotechconsultoria.com.br']
 
 # Application definition
 
