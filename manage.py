b887e9bbbaf605085c35ea5e398eb6ba0286aeb8
Henrique <henriquesg09@hotmail.com>
2019-12-05 05:49:30 -0300

    Setted develop settings

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biotech.settings.production')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biotech.settings.develop')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
